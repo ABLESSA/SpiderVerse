23f8fa4f7c3d02e63e5536e179c3c29525844c60
ABLESSA <18776505272@163.com>
2026-07-24 11:40:34 +0800

    Update webgl-renderer.js

--- src/webgl-renderer.js
+++ src/webgl-renderer.js
@@ -214,12 +214,10 @@ void main() {
       color = risoMisprint(v_uv);
     } else if (u_mode == 7) {
       color = blueprintInk(v_uv);
-       } else if (u_mode == 8) {
+          } else if (u_mode == 8) {
       color = newspaperHalftone(v_uv);
-    } else if (u_mode == 9) {
+    } else {
       color = glitchPrint(v_uv);
-    } else if (u_mode == 10) {
-      color = negative(v_uv);
     }
   }
 
